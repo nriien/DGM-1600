b2d8aed3c9913cccdfd23b12786c677400ec3447
unknown <10645056@CS514-19.ad.uvu.edu>
2015-09-02 10:06:30 -0600

    Variable and Functions Assignment

---
+++ Variables and Functions/Assets/VandF.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VandF : MonoBehaviour {
+	int Myint = 22;
+
+	void Start()
+	{
+		Myint = MultiplyByTwo(Myint);
+		Debug.Log (Myint);
+	}
+
+	int MultiplyByTwo(int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+
+}
