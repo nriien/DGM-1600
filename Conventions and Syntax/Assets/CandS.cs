9840cc525707e75503a1be44d8ad2bf7300ff6a1
unknown <10645056@CS514-19.ad.uvu.edu>
2015-09-09 09:43:36 -0600

    Conventions and Syntax Assignment

---
+++ Conventions and Syntax/Assets/CandS.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandS : MonoBehaviour{
+	private 
+{
+	float HeightTest = 3f;
+
+	void Start()
+		{
+		Debug.Log(transform);
+		if(transform <= HeightTest);
+		Debug.Log("I'm about to hit the ground!");
+		}
+			
+	void FlyAway()
+		{
+		if(transform >= HeightTest);
+		Debug.Log("I'm flying!");
+		}
+				
+	void FlyAway()
+		{
+		Debug.Log("Fly Away!");
+		Vector3 newPosition = new Vector3 (0, 7, 0);
+		transform.position = newPosition;
+		}
+}
