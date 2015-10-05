2b56b136af3756c1ab5d6e0508dfd486d6a0a301
unknown <10645056@CS514-19.ad.uvu.edu>
2015-10-05 09:24:27 -0600

    Scope and Access Modifiers

---
+++ Scope and Access Modifiers/Assets/Part1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Part1 : MonoBehaviour
+{
+	public int alpha = 5;
+	
+	
+	private int beta = 0;
+	private int gamma = 5;
+	
+	
+	private Part2 myOtherClass;
+	
+	
+	void Start ()
+	{
+		alpha = 29;
+		
+		myOtherClass = new Part2();
+		myOtherClass.FruitMachine (alpha, myOtherClass.apples);
+	}
+	
+	
+	void Example (int pens, int crayons)
+	{
+		int answer;
+		answer = pens * crayons * alpha;
+		Debug.Log(answer);
+	}
+	
+	
+	void Update ()
+	{
+		Debug.Log("Alpha is set to: " + alpha);
+	}
+}
---
+++ Scope and Access Modifiers/Assets/Part2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Part2 : MonoBehaviour
+
+{
+	public int apples;
+	public int bananas;
+	
+	public void FruitMachine (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Fruit total: " + answer);
+	}
+	
+	
+	private void OfficeSort (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Office Supplies total: " + answer);
+	}
+}
