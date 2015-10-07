ea56c81fc14732f986847798e000939220b73bac
unknown <10645056@CS514-19.ad.uvu.edu>
2015-10-07 09:48:34 -0600

    Classes

---
+++ Classes/Assets/Classes.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Classes : MonoBehaviour
+{
+	public class Stuff
+	{
+		public int bullets;
+		public int grenades;
+		public int rockets;
+		public float fuel;
+		
+		public Stuff()
+		{
+			bullets = 1;
+			grenades = 1;
+			rockets = 1;
+		}
+		
+		public Stuff(int bul, int gre, int roc)
+		{
+			bullets = bul;
+			grenades = gre;
+			rockets = roc;
+		}
+		
+		public Stuff(int bul, float fu)
+		{
+			bullets = bul;
+			fuel = fu;
+		}
+	}
+		
+		
+		
+	
+	
+	public Stuff myStuff = new Stuff(50, 5, 5);
+	
+	public Stuff myOtherStuff = new Stuff(50, 1.5f);
+	
+	void Start()
+	{
+		myOtherStuff = myStuff;
+		myOtherStuff.bullets = 100;
+		
+		int int1 = 5;
+		int int2 = int1;
+		int2 = 100;
+		
+		Debug.Log(myStuff.bullets); 
+		Debug.Log(int1); 
+	}
+}
